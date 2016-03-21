4fd674feee361198ab393908356bd059314026c2
Liangfu Chen <chenclf@gmail.com>
2016-03-21 23:39:24 +0800

    add icdar2003 dataset;

---
+++ data/icdar2003/get_icdar2003.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+wget http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTrain/scene.zip -O TrailTrain-scene.zip
+wget http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTest/scene.zip -O TrailTest-scene.zip
+
+# wget http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTrain/word.zip -O TrailTrain-word.zip
+# wget http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTest/word.zip -O TrailTest-word.zip
+
+# wget http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTrain/char.zip -O TrailTrain-char.zip
+# wget http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTest/char.zip -O TrailTest-char.zip
+
+
