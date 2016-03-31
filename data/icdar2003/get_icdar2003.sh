4479f756b08ba59385398d693ec2f495d40be81b
Liangfu Chen <liangfu.chen@nlpr.ia.ac.cn>
2016-03-31 15:36:05 +0800

    rename output filename;

--- data/icdar2003/get_icdar2003.sh
+++ data/icdar2003/get_icdar2003.sh
@@ -3,13 +3,13 @@
 # the following download links are publicly available at 
 # http://www.iapr-tc11.org/mediawiki/index.php/ICDAR_2003_Robust_Reading_Competitions
 
-wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTrain/scene.zip -O TrailTrain-scene.zip
-# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTest/scene.zip -O TrailTest-scene.zip
+wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTrain/scene.zip -O SceneTrailTrain.zip
+# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTest/scene.zip -O SceneTrailTest.zip
 
-# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTrain/word.zip -O TrailTrain-word.zip
-# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTest/word.zip -O TrailTest-word.zip
+# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTrain/word.zip -O WordTrailTrain.zip
+# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTest/word.zip -O WordTrailTest.zip
 
-# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTrain/char.zip -O TrailTrain-char.zip
-# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTest/char.zip -O TrailTest-char.zip
+# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTrain/char.zip -O CharTrailTrain.zip
+# wget -c http://www.iapr-tc11.org/dataset/ICDAR2003_RobustReading/TrialTest/char.zip -O CharTrailTest.zip
 
 
