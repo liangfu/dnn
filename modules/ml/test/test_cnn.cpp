45df05df750faec71ddcbe0485c2c1fa72167bdb
Liangfu Chen <chenclf@gmail.com>
2016-04-25 01:17:51 +0800

    implementing gradient checking for activation layers;

---
+++ modules/ml/test/test_cnn.cpp
@@ -0,0 +1,171 @@
+
+#include "test_precomp.hpp"
+
+#include <string>
+#include <fstream>
+#include <iostream>
+
+#include "cvext_c.h"
+
+// using namespace std;
+
+class CV_NetworkTest : public cvtest::BaseTest
+{
+public:
+  CV_NetworkTest();
+  ~CV_NetworkTest();
+
+protected:
+  void run(int);
+
+  int TestTrainPredict(int test_num);
+  int TestSaveLoad();
+
+  int checkPredictError(int test_num);
+  int checkLoadSave();
+
+  // CvNetwork * network;
+};
+
+CV_NetworkTest::CV_NetworkTest()
+{
+}
+
+CV_NetworkTest::~CV_NetworkTest()
+{
+}
+
+int CV_NetworkTest::TestTrainPredict(int test_num)
+{
+  int code = cvtest::TS::OK;
+  // if (!tmp_code){
+  //   ts->printf( cvtest::TS::LOG, "Model training was failed.");
+  //   return cvtest::TS::FAIL_INVALID_OUTPUT;
+  // }
+  code = checkPredictError(test_num);
+  return code;
+}
+
+int CV_NetworkTest::checkPredictError(int test_num)
+{
+  // if (!gtb){
+  //   return cvtest::TS::FAIL_GENERIC;
+  // }
+  // if ( abs( current_error - mean[test_num]) > 6*sigma[test_num] ){
+  //   ts->printf( cvtest::TS::LOG, "Test error is out of range:\n"
+  //               "abs(%f/*curEr*/ - %f/*mean*/ > %f/*6*sigma*/",
+  //               current_error, mean[test_num], 6*sigma[test_num] );
+  //   return cvtest::TS::FAIL_BAD_ACCURACY;
+  // }
+  return cvtest::TS::OK;
+}
+
+
+int CV_NetworkTest::TestSaveLoad()
+{
+  int code = cvtest::TS::OK;
+  // if (!gtb){
+  //   return cvtest::TS::FAIL_GENERIC;
+  // }
+  // model_file_name1 = cv::tempfile();
+  // model_file_name2 = cv::tempfile();
+  // gtb->save(model_file_name1.c_str());
+  // gtb->calc_error(data, CV_TEST_ERROR, &test_resps1);
+  // gtb->load(model_file_name1.c_str());
+  // gtb->calc_error(data, CV_TEST_ERROR, &test_resps2);
+  // gtb->save(model_file_name2.c_str());
+
+  return checkLoadSave();
+}
+
+int CV_NetworkTest::checkLoadSave()
+{
+  int code = cvtest::TS::OK;
+
+  // 1. compare files
+  // delete temporary files
+  // remove( model_file_name1.c_str() );
+  // remove( model_file_name2.c_str() );
+
+  // 2. compare responses
+  // CV_Assert( test_resps1.size() == test_resps2.size() );
+  // vector<float>::const_iterator it1 = test_resps1.begin(), it2 = test_resps2.begin();
+  // for( ; it1 != test_resps1.end(); ++it1, ++it2 ){
+  //   if( fabs(*it1 - *it2) > FLT_EPSILON ){
+  //     ts->printf( cvtest::TS::LOG,
+  //                 "Responses predicted before saving and after loading are different" );
+  //     code = cvtest::TS::FAIL_INVALID_OUTPUT;
+  //   }
+  // }
+  return code;
+}
+
+
+
+void CV_NetworkTest::run(int)
+{
+  std::string dataPath = std::string(ts->get_data_path());
+  int code = cvtest::TS::OK;
+
+  // for (int i = 0; i < 4; i++){
+  //   int temp_code = TestTrainPredict(i);
+  //   if (temp_code != cvtest::TS::OK){
+  //     code = temp_code;break;
+  //   }else if (i==0){
+  //     temp_code = TestSaveLoad();
+  //     if (temp_code != cvtest::TS::OK){code = temp_code;}
+  //     delete data;
+  //     data = 0;
+  //   }
+  //   delete gtb;gtb = 0;
+  // }
+  // delete data;
+  // data = 0;
+
+  ts->set_failed_test_info( code );
+}
+
+/////////////////////////////////////////////////////////////////////////////
+//////////////////// test registration  /////////////////////////////////////
+/////////////////////////////////////////////////////////////////////////////
+
+// TEST(ML_Network, accuracy) { CV_NetworkTest test; test.safe_run(); }
+
+TEST(ML_Tanh, accuracy)
+{
+  int nr=5,nc=5;
+  const float delta = 1e-5f;
+  CvMat * src = cvCreateMat(nr,nc,CV_32F);
+  CvMat * src_more = cvCreateMat(nr,nc,CV_32F);
+  CvMat * src_less = cvCreateMat(nr,nc,CV_32F);
+  CvMat * dst = cvCreateMat(nr,nc,CV_32F); 
+  CvMat * dst_more = cvCreateMat(nr,nc,CV_32F); 
+  CvMat * dst_less = cvCreateMat(nr,nc,CV_32F); 
+  CvMat * diff = cvCreateMat(nr,nc,CV_32F); 
+  CvMat * grad = cvCreateMat(nr,nc,CV_32F); 
+  CvRNG rng = cvRNG(-1);
+  cvRandArr(&rng,src,CV_RAND_UNI,cvScalar(-5),cvScalar(5));
+  cvAddS(src,cvScalar(delta),src_more);
+  cvAddS(src,cvScalar(-delta),src_less);
+  cvZero(dst); cvZero(dst_more); cvZero(dst_less);
+  cvTanh(src,dst);
+  cvTanh(src_more,dst_more);
+  cvTanh(src_less,dst_less);
+  cvSub(dst_more,dst_less,diff);
+  cvScale(diff,grad,1./(2.f*delta));
+  cvPrintf(stderr,"%.2f ",diff); fprintf(stderr,"--\n");
+  cvPrintf(stderr,"%.2f ",grad);
+  EXPECT_LT(cvAvg(grad).val[0], delta);
+}
+
+
+
+
+
+
+
+
+
+
+
+
